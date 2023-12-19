8013ecac82809f153b66e03d5e35e5087940f0cc
wanderfulife <john.wander@hotmail.fr>
2023-12-19 16:06:48 +0100

    psote

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <script setup>
-import Prenom from './Prenom.vue';
+import { ref } from 'vue';
+import Prenom from './Components/Prenom.vue';
+import Question1 from './Components/Question1.vue';
+
+
+const prenom = ref(null);
+const date = ref(null);
+const poste = ref(null)
+
+const onFormSubmitted = (data) => {
+  prenom.value = data.name;
+  date.value = data.date;
+};
+
+const onPostSubmitted = (data) => {
+  poste.value = data.poste;
+  alert(poste.value + date.value + prenom.value)
+}
+
 </script>
 
 <template>
-  <Prenom />
+  <div class="container">
+    <div v-if="!prenom">
+      <Prenom @formSubmitted="onFormSubmitted" />
+    </div>
+    <div v-else-if="!poste">
+      <Question1 @formSubmitted="onPostSubmitted"/>
+    </div>
+    <div v-else>
+      <h1>question2</h1>
+    </div>
+  </div>
 </template>
 
-
 <style>
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  font-size: 1.5rem;
+* {
+  margin: 0;
+  font-family:  Arial;
   background-color: #343541;
+  font-size: 1.1em;
+  color: beige;
+  font-weight: bold;
 }
+
+input {
+  border: 0;
+  outline: 0;
+	color: grey;
+}
+.container {
+  display: flex;
+}
+
 </style>
---
+++ src/Components/Prenom.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>Nom Enqueteur :</h1>
+	<form autocomplete="off"  @submit.prevent="saveName">
+		<input autofocus type="text" v-model="name" required />
+	</form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const date = ref('');
+const emit = defineEmits(['formSubmitted']);
+
+const saveName = () => {
+	date.value = new Date().toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
+	emit('formSubmitted', { name: name.value, date: date.value });
+};
+</script>
